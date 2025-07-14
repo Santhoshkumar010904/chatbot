6ba9df9bf03a0f1d62b3eaa99618b1584ab4234d
Santhoshkumar010904 <msanthoshkumar229@gmail.com>
2025-07-14 22:50:35 +0530

    hello

--- frontend/uic/vite.config.js
+++ frontend/uic/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Optional: ensures relative paths in build
+  build: {
+    rollupOptions: {
+      external: ['uuid']
+    }
+  }
 });
